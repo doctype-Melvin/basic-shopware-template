1c250784d9ce1c654fb9c6261aa8a16ba0fcc9bc
Melvin Speckamp <doctype.melvin@gmail.com>
2023-09-07 10:13:54 +0200

    initial commit

---
+++ src/MyNewPlugin.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace MyNewPlugin;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class MyNewPlugin extends Plugin implements ThemeInterface
+{
+}
